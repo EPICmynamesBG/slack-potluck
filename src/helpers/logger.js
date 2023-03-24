6d1fef082055785e9dfb70e9a3251d9cdfada603
EPICmynamesBG <EPICmynamesBG@users.noreply.github.com>
2023-03-24 00:21:59 -0400

    always enable Console logger

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -12,19 +12,15 @@ function getLoggerInstance(serviceName) {
       //
       new winston.transports.File({ filename: "error.log", level: "error" }),
       new winston.transports.File({ filename: "combined.log" }),
+      new winston.transports.Console({
+        format: winston.format.simple(),
+      }),
     ],
   });
 
-  if (process.env.NODE_ENV !== "production") {
-    localInstance.add(
-      new winston.transports.Console({
-        format: winston.format.simple(),
-      })
-    );
-  }
   localInstance.getLevel = function () {
     return this.level;
-  }
+  };
   return localInstance;
 }
 
