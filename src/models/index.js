922147f2fbc3d510d1768e83ac8c78530a662f34
EPICmynamesBG <EPICmynamesBG@users.noreply.github.com>
2023-03-25 23:57:43 -0400

    db logging

--- src/models/index.js
+++ src/models/index.js
@@ -4,12 +4,19 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+const { getInstance } = require('../helpers/logger');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.json')[env];
 const db = {};
 
+const logger = getInstance('db');
+
 let sequelize;
+Object.assign(config, {
+  logging: logger.debug.bind(logger)
+});
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
