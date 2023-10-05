36b761b596676201782a653df68deda51f7119af
EPICmynamesBG <EPICmynamesBG@users.noreply.github.com>
2023-10-05 19:12:09 -0400

    revert docker-compose changes

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-        name: 'slack-potluck-server',
-        script: './src/app.js',
-        env_production: {
-            NODE_ENV: 'production'
-        }
-    }]
-}
